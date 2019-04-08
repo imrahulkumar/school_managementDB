1710f57ba2718a021149d460f7dd6114e7aeca9f
Rahul Kumar <rahul151995@gmail.com>
2019-04-08 23:52:24 +0530

    issue resolved

--- app/modules/student/controllers/user.controller.js
+++ app/modules/student/controllers/user.controller.js
@@ -18,30 +18,6 @@ const student_registration = async function student_registration(req,res){
 
 
 
-// login an existing user and generate refresh jwt token
-// const user_login = async function user_login(req, res) {
-//     try {
-//         const result = await login(req.body);
-//         res.status(200).send(result);
-//     }
-//     catch (e) {
-//         res.status(400).send(errorHandler(e));
-//     }
-// }
-
-// const user_verify = async function user_verify(req, res) {
-//     try {
-//         const result = await verify(req, res);
-//         res.status(200).send(result);
-//     }
-//     catch (e) {
-//         res.status(400).send(errorHandler(e));
-//     }
-// }
-
-
-// module.exports = {
-//     user_register,
-//     user_login,
-//     user_verify
-// };
+module.exports = {
+    student_registration
+};
--- app/modules/student/routes/student.routes.js
+++ app/modules/student/routes/student.routes.js
@@ -1,9 +1,8 @@
 const { Router } = require("express"),
     router = Router(),
-    { user_register, user_login, user_verify } = require("../../student/controllers/user.controller");
+    { student_registration} = require("../../student/controllers/user.controller");
 
-router.post("/registration", user_register);
-// router.post("/login", user_login);
-// router.get("/verify", user_verify);
+router.post("/registration", student_registration);
+
 
 module.exports = router
